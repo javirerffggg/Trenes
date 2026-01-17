5fbb1c427e6f188ac3d273ae9d0a3ae15a020135
javirerffggg <160790690+javirerffggg@users.noreply.github.com>
2026-01-18 00:08:20 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      // BASE: Es crucial para GitHub Pages. Usa './' para que las rutas sean relativas
+      // al archivo index.html, independientemente del nombre del repositorio.
+      base: './', 
       server: {
         port: 3000,
         host: '0.0.0.0',
